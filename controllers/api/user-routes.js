1bda44da24e0cd82f882fd88d7e576cb2d1f157a
Trey S <fjschmidt292@gmail.com>
2022-07-25 00:52:40 -0400

    templating done, wrote preliminary routes, adding functionality and testing

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,105 @@
+const router = require('express').Router();
+const sequelize = require('../../config/connection');
+const { User, Post, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.get('/', (req, res) => {
+    User.findAll({
+        attributes: { exclude: ['password'] }
+    })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.get('/:id', (req, res) => {
+    User.findOne({
+    attributes: { exclude: ['password'] },
+    where: {
+      id: req.params.id
+    },
+  include: [
+      {
+        model: Post,
+        attributes: ['id', 'title', 'text_content', 'created_at']
+      },
+      {
+        model: Comment,
+     attributes: ['id', 'comment_text', 'created_at'],
+        include: {
+        model: Post,
+          attributes: ['title']
+        }
+      }
+    ]
+  })
+  .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No user with this ID found' });
+        return;
+      }
+    res.json(dbUserData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
+router.post('/', (req, res) => {
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  })
+  .then(dbUserData => {
+    req.session.save(() => {
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+
+      res.json(dbUserData);
+    });
+  });
+});
+
+router.post('/login', (req, res) => {
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  }).then(dbUserData => {
+    if (!dbUserData) {
+      res.status(400).json({ message: 'No user with that email address' });
+    return; 
+      }
+
+      const validPassword = dbUserData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res.status(400).json({ message: 'Error, wrong password' });
+      return;
+    }
+
+    req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+      res.json({ user: dbUserData, message: 'You are now logged in' });
+    });
+  });
+});
+
+router.post('/logout', withAuth, (req,res) => {
+  if (req.session.loggedIn) {
+      req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,79 +1,69 @@
-const { Model, DataTypes } = require('sequelize');
-const bcrypt = require('bcrypt');
-const sequelize = require('../config/connection');
-// create the user model
+// import all models
+const Post = require('./Post');
+const User = require('./User');
+const Vote = require('./Vote');
+const Comment = require('./Comment');
 
-class User extends Model {
-    // set up method to run on instance data (per user) to check password
-    checkPassword(loginPw) {
-        return bcrypt.compareSync(loginPw, this.password);
-    }
-}
+// create associations
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
 
-// define table columns and configuration
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
 
-// create fields/columns for User model
-User.init(
-    {
-    //table column definitions go here.
-      id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
-      },
-//define a username column
-      username: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-//define an email column
-      email: {
-        type: DataTypes.STRING,
-//there cannot be any duplicate email values in this table        
-        allowNull: false,
-        unique: true,
-// if allowNull is set to false, we can run our data through validators before creating the table data
-        validate: {
-          isEmail: true
-        }
-      },
-    //define a password column
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-    // this means the password must be at least 4 characters long
-          len: [4]
-        }
-      }
-    },
-    {
-      hooks: {
-        // set up beforeCreate lifecycle "hook" functionality
-        async beforeCreate(newUserData) {
-          newUserData.password = await bcrypt.hash(newUserData.password, 10);
-          return newUserData;
-        },
-    // set up before update lifecycle "hook" functionality
-        async beforeUpdate(updatedUserData) {
-          updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-          return updatedUserData;
-        }
-      },
-      //TABLE CONFIGURATIONS OPTIONS GO HERE (https:// sequelize.org/v5/manual/models-definition.html#configuration)
+User.belongsToMany(Post, {
+  through: Vote,
+  as: 'voted_posts',
 
-    //pass in our imported sequelize connection (the direct connection to our database)
-      sequelize,
-      //don't automatically create createdAt/updatedAt timestamp fields
-      timestamps: false,
-      //don't pluralize name of database table
-      freezeTableName: true,
-      // use underscores instead of came-casing(i.e. 'comment_text and not 'commentText'.)
-      underscored: true,
-      //make it so our model name stays lowercase in the database
-      modelName: 'user'
-    }
-  );
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
 
-module.exports = User;
+Post.belongsToMany(User, {
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'post_id',
+  onDelete: 'SET NULL'
+});
+
+Vote.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+Vote.belongsTo(Post, {
+  foreignKey: 'post_id',
+  onDelete: 'SET NULL'
+});
+
+User.hasMany(Vote, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Vote, {
+  foreignKey: 'post_id'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+  onDelete: 'SET NULL'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote, Comment };
